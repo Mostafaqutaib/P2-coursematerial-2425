6987b6d84ccb60e272d5a65df8443975aaea0b50
Mostafa Qutaib <YOUR_GITHUB_EMAIL@example.com>
2025-10-02 12:30:22 +0200

    fixing the 3d assignment of 04-bmi-calculator

--- 01-oo/03-properties/assignments/04-bmi-calculator/student.py
+++ 01-oo/03-properties/assignments/04-bmi-calculator/student.py
@@ -1,1 +1,27 @@
 # Write your code here
+class BMICalculator:
+    def __init__(self, weight_in_kg, height_in_m):
+        self.__weight_in_kg = weight_in_kg
+        self.__height_in_m = height_in_m
+    
+    @property
+    def weight_in_kg(self):
+        return self.__weight_in_kg
+    @property
+    def height_in_m(self):
+        return self.__height_in_m
+    @property
+    def bmi(self):
+        return self.__weight_in_kg / self.__height_in_m**2
+
+    @property
+    def category(self):
+        if self.bmi < 18.5:
+            return 'underweight'
+        elif 18.5 <= self.bmi < 25:
+            return 'normal'
+        else:
+            return 'overweight'
+calc = BMICalculator(weight_in_kg=90, height_in_m=1.80)
+calc.bmi
+calc.category
