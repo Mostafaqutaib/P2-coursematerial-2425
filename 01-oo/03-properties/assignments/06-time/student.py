1dfc15093e438e35d6a79d2a35e48d917e78a5ce
Mostafa Qutaib <YOUR_GITHUB_EMAIL@example.com>
2025-10-02 14:45:06 +0200

    Time (validated setters). All tests passing

--- 01-oo/03-properties/assignments/06-time/student.py
+++ 01-oo/03-properties/assignments/06-time/student.py
@@ -1,1 +1,49 @@
 # Write your code here
+class Time:
+    def __init__(self, hours, minutes, seconds):
+        self.__hours = 0
+        self.__minutes = 0
+        self.__seconds = 0
+        
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+
+    @property
+    def hours(self):
+        return self.__hours
+        
+    @hours.setter
+    def hours(self, value):
+        if value < 0 or value > 23:
+            raise ValueError("hours must be between 0 & 23")
+        else:
+            self.__hours = value
+
+        
+    @property
+    def minutes(self):
+        return self.__minutes
+        
+    @minutes.setter
+    def minutes(self, value):
+        if value < 0 or value > 59:
+            raise ValueError("minutes must be between 0 & 59")
+        else:
+            self.__minutes = value
+                
+        
+    @property
+    def seconds(self):
+        return self.__seconds
+        
+    @seconds.setter
+    def seconds(self, value):
+        if value < 0 or value > 59:
+            raise ValueError("seconds must be between 0 & 59")
+        else:
+            self.__seconds = value
+                
+        
+        
