058645a7b05f8847fa317df673bc952a6db25f98
Mostafa Qutaib <mostafaqutaib.mohammed@student.ucll.be>
2025-12-01 06:47:09 +0100

    solution regex

--- 04-regex/01-matching/17-contains-three-digits/student.py
+++ 04-regex/01-matching/17-contains-three-digits/student.py
@@ -1,2 +1,10 @@
 
 # Write your code here
+# Write your code here
+import re
+
+def contains_three_digits(string):
+    patteren = r".*[0-9].*[0-9].*[0-9].*"
+
+    if re.fullmatch(patteren, string):
+        return True
